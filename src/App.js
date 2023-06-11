80cc17646e78734dea569c2bfd195dbe2c43f82d
Mahad Asif <mahad.asif.git@gmail.com>
2023-06-11 21:35:43 +0500

    added useReducer hook.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Card from './components/Card';
 import NavBar from './components/NavBar';
 import UploadForm from './components/UploadForm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 const photos = [
   'https://picsum.photos/id/1001/200/200',
@@ -14,50 +14,80 @@ const photos = [
 ]
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [input, setInput] = useState({title: null, file: null, path: null});
-  const [items, setItems] = useState(photos);
-  const [isCollapsed, collapse] = useState(false);
 
-  const toggle = () => collapse(!isCollapsed);
+  const initialState = {
+    count: 0,
+    input: { title: null, file: null, path: null },
+    items: photos,
+    isCollapsed: false
+  }
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'setCount':
+        return {
+          ...state,
+          count: action.payload.count
+        }
+      case 'setItems':
+        return {
+          ...state,
+          items: [action.payload.path, ...state.items]
+        }
+      case 'setInput':
+        return {
+          ...state,
+          input: { ...state.input, ...action.payload }
+        }
+      case 'setCollapse':
+        return {
+          ...state,
+          isCollapsed: !state.isCollapsed
+        }
+      default: return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const toggle = () => dispatch({ type: 'setCollapse', payload: { collapse: !state.isCollapsed } });
 
   const handleOnChange = (e) => {
     if (e.target.type === 'file') {
-      setInput({ ...input, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])});
+      dispatch({ type: 'setInput', payload: { file: e.target.files[0], path: URL.createObjectURL(e.target.files[0]) } });
     } else {
-      setInput({ ...input, title: e.target.value });
+      dispatch({ type: 'setInput', payload: { title: e.target.value } });
     }
   }
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    setItems([input.path, ...items]);
-    setInput({title: null, file: null, path: null});
-    collapse(false);
+    dispatch({ type: 'setItems', payload: { path: state.input.path } });
+    dispatch({ type: 'setInput', payload: { input: initialState.input } });
+    toggle;
   };
 
-  // takes effect only when change occurs to items ...
   useEffect(() => {
-    setCount(items.length);
-  }, [items]);
+    dispatch({ type: 'setCount', payload: { count: state.items.length } })
+  }, [state.items]);
 
   return (
     <>
       <div className="App">
         <NavBar />
         <div className='container text-center mt-5'>
-          {`You have ${count} image${items.length > 1 ? 's' : ''}`}
+          {`You have ${state.count} image${state.items.length > 1 ? 's' : ''}`}
           <h1>Gallery</h1>
-          <button className='btn btn-success float-end' onClick={toggle}>{isCollapsed ? 'close' : '+ add'}</button>
-          { isCollapsed &&
-              <UploadForm
-                input={input}
-                onChange={handleOnChange}
-                onSubmit={handleOnSubmit} 
-              />
+          <button className='btn btn-success float-end' onClick={toggle}>{state.isCollapsed ? 'close' : '+ add'}</button>
+          {state.isCollapsed &&
+            <UploadForm
+              input={state.input}
+              onChange={handleOnChange}
+              onSubmit={handleOnSubmit}
+            />
           }
           <div className='row'>
-            {items.map((photo, index) => <Card key={index} src={photo} />)}
+            {state.items.map((photo, index) => <Card key={index} src={photo} />)}
           </div>
         </div>
       </div>
